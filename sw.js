527190f1c2088f72e4e0c88a8ca9dc3c8fbf7daa
m7-bytes <mailby.m7@gmail.com>
2025-09-01 22:31:02 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "bill-tracker-cache-v1";
+const ASSETS = [
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "https://cdn.jsdelivr.net/npm/chart.js" 
+];
+
+// Install Service Worker
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate Service Worker
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch from cache or network
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
